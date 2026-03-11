fe86b7eab1c1635601a4eb346d49b0c9fb80265f
keerthi9694 <keerthijaru106@gmail.com>
2026-03-11 21:36:18 +0530

    Added c# console apps

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hasta la vista!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int empno;
+            String ename;
+            double salary;
+            String job;
+
+            Console.Write("Enter Employee Number: ");
+            empno = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Employee Name: ");
+            ename = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            salary = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Job: ");
+            job = Console.ReadLine();
+
+            Console.WriteLine("EmpNo: " + empno);
+            Console.WriteLine("Ename: " + ename);
+            Console.WriteLine("salary: " + salary);
+            Console.WriteLine("Job: " + job);
+
+
+
+
+
+        }
+    }
+}
