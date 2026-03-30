0e5772edbe6573b74b4b5610556c1fa0293bd0b8
keerthi9694 <keerthijaru106@gmail.com>
2026-03-30 23:30:49 +0530

    Added day-28 MVC project

---
+++ week-6/day-28/WebApplication1/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        
+        //public int Empno { get; set; }
+        //public string Ename { get; set; }
+        //public string Job { get; set; }
+        //public decimal Salary { get; set; }
+        //public int Deptno { get; set; }
+
+
+        [Required(ErrorMessage = "Empno is required")]
+        public int Empno { get; set; }
+
+        [Required(ErrorMessage = "Employee Name is required")]
+        [StringLength(15, MinimumLength = 5)]
+        public string Ename { get; set; }
+
+
+        public string Job { get; set; }
+
+        [Range(10000, 100000)]
+        public decimal Salary { get; set; }
+
+        [Required(ErrorMessage = "Dept Number is required")]
+        public int Deptno { get; set; }
+
+
+
+
+
+    }
+}
